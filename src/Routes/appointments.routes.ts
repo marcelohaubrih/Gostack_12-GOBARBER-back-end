39a6c7e6333fd4787e8211c379b7422370717364
Marcelo Haubrih <marcelohaubrih@gmail.com>
2020-06-23 00:17:10 -0300

    Última Aula concluída

--- src/Routes/appointments.routes.ts
+++ src/Routes/appointments.routes.ts
@@ -1,30 +1,37 @@
 import { Router, response } from 'express';
-import { uuid } from 'uuidv4';
-import { startOfHour, parseISO, isEqual } from 'date-fns';
-import Appointment from '../Models/Appointment';
+import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
-
+import CreateAppointementService from '../Services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentsRouter.get('/', (request, response) => {
+  try {
+    const appointments = appointmentsRepository.all();
+    return response.json(appointments);
+  } catch (error) {
+    return response.status(500).json({
+      message: "Fatal Error!"
+    });
+  }
+})
+
 appointmentsRouter.post('/', (request, response) => {
+  try {
     const { provider, date } = request.body;
-    const parsedDate = startOfHour(parseISO(date));
-
-    const findAppointmentInSameDate = appointmentsRepository.findByDate(parsedDate);
-
-    if(findAppointmentInSameDate){
-        return response.status(400).json({
-            message: "This appointment is already booked"
-        })
-    }
-
-    const appointment = appointmentsRepository.create(provider, parsedDate);
+    const parsedDate = parseISO(date);
+    const createAppointment = new CreateAppointementService(appointmentsRepository);
+    const appointment = createAppointment.execute({date: parsedDate, provider})
 
     return response.json({
         appointment
     });
+  } catch (error) {
+    return response.status(400).json({
+      message: error.message
+    });
+  };
 });
 
 export default appointmentsRouter;
--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -1,6 +1,12 @@
 import Appointment from '../Models/Appointment';
 import { isEqual } from 'date-fns';
 
+//Data Transfer Object
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
 class AppointmentsRepository {
  private appointments : Appointment[];
 
@@ -8,14 +14,19 @@ class AppointmentsRepository {
    this.appointments = [];
  }
 
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
   public findByDate(date: Date): Appointment | null {
     const findAppointment = this.appointments.find(appointment => isEqual(date, appointment.date));
 
     return findAppointment || null;
   }
 
-  public create(provider: string, date: Date): Appointment {
-    const appointment = new Appointment(provider, date);
+  //provider: string, date: Date
+  public create({date, provider}: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({provider, date});
 
     this.appointments.push(appointment);
 
