352d923de5b93ddb161a22d5a139baf467c35f6e
Marcelo Haubrih <marcelohaubrih@gmail.com>
2020-07-13 16:14:00 -0300

    add: Camada DDD - Reescrevendo Repositorios

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -11,23 +11,23 @@ interface Request {
 }
 
 class CreateAppointmentService {
-  public async execute({date, provider_id}: Request): Promise<Appointment> {
+  public async execute({ date, provider_id }: Request): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(appointmentDate);
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate,
+    );
 
-    if(findAppointmentInSameDate){
-      throw new AppError("This appointment is already booked");
-    };
+    if (findAppointmentInSameDate) {
+      throw new AppError('This appointment is already booked');
+    }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = await appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
-
-    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
