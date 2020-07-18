7e4ae2bcb6323e6ac626b593eea05a3667ef9ea6
Marcelo Haubrih <marcelohaubrih@gmail.com>
2020-07-18 01:38:20 -0300

    fix paths

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -8,11 +8,10 @@ export default {
   directory: tmpFolder,
   storage: multer.diskStorage({
     destination: tmpFolder,
-    filename(request, file, callback){
+    filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
       const fileName = `${fileHash}-${file.originalname}`;
       return callback(null, fileName);
-    }
+    },
   }),
-
 };
