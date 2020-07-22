0daae751af2d529c999b458c96765620052c5d08
Marcelo Haubrih <marcelohaubrih@gmail.com>
2020-07-22 00:22:34 -0300

    Aula 17 Nivel 5 - criacao agendamento

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -15,6 +15,7 @@ describe('CreateAppointment', () => {
   it('should be able to create a new appointment', async () => {
     const appointment = await createAppointment.execute({
       date: new Date(),
+      user_id: '123456789',
       provider_id: '123456789',
     });
 
@@ -26,12 +27,14 @@ describe('CreateAppointment', () => {
     const appointmentDate = new Date();
     await createAppointment.execute({
       date: appointmentDate,
+      user_id: '123456789',
       provider_id: '123456789',
     });
 
     expect(
       createAppointment.execute({
         date: appointmentDate,
+        user_id: '123456789',
         provider_id: '123456789',
       }),
     ).rejects.toBeInstanceOf(AppError);
